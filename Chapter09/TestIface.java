99555870bab8b3acbc2374ee237dbbe94ae03321
Igor Bahtin <ibahtin@gmail.com>
2015-06-04 14:07:41 +0300

    Chapter 09 passed.

---
+++ Chapter09/TestIface.java
@@ -0,0 +1,17 @@
+interface Callback {
+  void callback(int param);
+}
+
+class Client implements Callback {
+  // Implement Callback's interface
+  public void callback(int p) {
+    System.out.println("callback called with " + p);
+  }
+}
+
+class TestIface {
+  public static void main(String args[]) {
+    Callback c = new Client();
+    c.callback(42);
+  }
+}
